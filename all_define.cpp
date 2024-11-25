a5af3714f5cd6dd5c9013957fbf60625426415f1
subrata das <sd7184732@gmail.com>
2024-11-26 00:52:41 +0600

    Add files via upload

---
+++ all_define.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+#define test int t;cin >> t;while (t--)
+#define ll long long
+using namespace std;
+void solve() {
+
+    ll a;
+    cin >> a;
+    cout << a << endl;
+}
+
+int main() {
+
+    solve();
+
+    return 0;
+}
