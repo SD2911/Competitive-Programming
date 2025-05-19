c7e79f87b1029e9dd3f6600ab42c39324090e9a9
subrata das <sd7184732@gmail.com>
2025-05-20 00:23:39 +0600

    Create basic.cpp

---
+++ ⛳STL/queue/basic.cpp
@@ -0,0 +1,32 @@
+#
+ queue<int> q;
+	    
+	   q.push(1);
+	    q.push(2);
+	     q.push(3);
+	      q.push(4);
+	       q.push(5);
+   cout<<q.size()<<endl;  // 5
+  // print all element:
+     while(!q.empty()){
+	          cout<<q.front()<<endl;
+	          q.pop();
+	      }
+	       
+	       cout<<q.front()<<endl;   // 1 
+	       q.pop();
+	       
+	       cout<<q.front()<<endl;  // 2
+
+#
+
+
+
+
+
+
+
+
+
+
+
