b703e7b258f1109c069be3572e672260cdb4e12b
subrata das <sd7184732@gmail.com>
2025-05-24 00:27:39 +0600

    Update basic.cpp

--- ⛳STL/priority queue/basic.cpp
+++ ⛳STL/priority queue/basic.cpp
@@ -1,1 +1,17 @@
-
+#
+priority_queue<int> p;
+        
+        p.push(1);
+        p.push(2);
+        p.push(4);
+        p.push(3);
+        
+    //priority_queue will give priority to greater value
+    
+     while(!p.empty()){
+         cout<<p.top()<<" ";
+         p.pop();
+     }
+     cout<<endl;  // 4 3 2 1
+     
+     cout<<p.size()<<endl;   //0
