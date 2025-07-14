13902ab7b4aff6f0d0ee174ad6b0179d89394045
subrata das <sd7184732@gmail.com>
2025-07-14 22:00:34 +0600

    Update frog.cpp

--- DSA/DP/frog.cpp
+++ DSA/DP/frog.cpp
@@ -1,30 +1,20 @@
-There are N stones, numbered 1,2,…,N. For each i (1≤i≤N), the height of Stone i is h i​ .
+There are N stones, numbered 1,2,…,N. For each i (1≤i≤N), the height of Stone i is h i
+.
+There is a frog who is initially on Stone 1. He will repeat the following action some number of times to reach Stone N:
 
-There is a frog who is initially on Stone 1. He will repeat the following action some number of times to reach Stone 
-N:
-
-If the frog is currently on Stone i, jump to one of the following: Stone 
-i+1,i+2,…,i+K. Here, a cost of ∣h i​ −h j​ ∣ is incurred, where j is the stone to land on.
-Find the minimum possible total cost incurred before the frog reaches Stone 
+If the frog is currently on Stone i, jump to Stone i+1 or Stone i+2. Here, a cost of ∣h i −h j
+​ ∣ is incurred, where j is the stone to land on.Find the minimum possible total cost incurred before the frog reaches Stone 
 N.
 
 Constraints
-All values in input are integers.
-2≤N≤10 
-5
- 
-1≤K≤100
-1≤h 
-i
-​
- ≤10 
-4
+All values in input are integers.2≤N≤10 5
+ 1≤h i
+​ ≤10 4
  
 Input
 Input is given from Standard Input in the following format:
 
-N 
-K
+N
 h 
 1
 ​
@@ -43,16 +33,18 @@ Print the minimum possible total cost incurred.
 
 Sample Input 1
 Copy
-5 3
-10 30 40 50 20
+4
+10 30 40 20
 Sample Output 1
 Copy
 30
 If we follow the path 
 1 → 
 2 → 
-5, the total cost incurred would be 
+4, the total cost incurred would be 
 ∣10−30∣+∣30−20∣=30.
+
+
 
 
 
