572109019847fbd4778100b9620fb619be3f650b
subrata das <sd7184732@gmail.com>
2026-01-10 23:59:07 +0600

    Add files via upload

---
+++ DSA/binary search/Codeforces_Queries about less or equal elements.cpp
@@ -0,0 +1,36 @@
+#include <bits/stdc++.h>
+#define test int t; cin >> t; while(t--)
+#define fast {ios_base::sync_with_stdio(0);cin.tie(0);}
+#define ll long long
+#define vl vector<ll>
+#define f(i,n) for (ll i = 0; i < n; i++)
+#define ia(a,n) vector<ll>a(n); f(i,n) cin>>a[i]
+#define pr_array f(i,n){cout<<a[i]<<" ";}cout<<endl;
+#define pauruti priority_queue<ll,vl,greater<ll>>
+#define all(a) a.begin(),a.end()
+#define mp map<ll,ll>
+#define lp pair<ll,ll>
+#define llms map<string,ll>
+#define yes cout << "YES\n"
+#define no cout << "NO\n"
+#define endl '\n'
+#define pb push_back
+#define lcm (a * b) / __gcd(a,b);
+#define PI acos(-1)
+const int mod = 1e9 + 7;
+using namespace std;
+int main(){
+    fast
+     // test{
+              int n,m;
+              cin >> n >> m;
+              ia(a,n);
+              ia(b,m);
+              sort(all(a));
+              f(i,m){
+                  cout<<upper_bound(all(a),b[i])-a.begin()<<" ";
+              }
+              cout<<endl;
+       // }
+      return 0;
+    }
